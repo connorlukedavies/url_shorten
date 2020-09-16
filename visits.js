b9ef9c043c45bc233805e11bd0eeee000a491966
connorlukedavies <67103903+connorlukedavies@users.noreply.github.com>
2020-09-16 02:37:00 +0100

    Delete visits.js

--- visits.js
+++
@@ -1,8 +0,0 @@
-function toggleStats() { //a toggle function to make a div appear and reappear
-    var x = document.getElementById("stats"); //locates the div with the id of "stats" and assigns it x
-    if (x.style.display === "block") { //if the div (x) is diplay:none
-      x.style.display = "none";    //then it will change it to block so it appears
-    } else {
-      x.style.display = "block"; // if already styled as block then it will change to none to make it dissapear
-    }
-  }
